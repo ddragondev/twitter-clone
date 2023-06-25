6626b7f6fb5f60107c113ca0b36e3452bee19d37
Diego Vega <dvegamed@gmail.com>
2023-06-24 22:47:42 -0400

    Tweet component

--- components/Tweet/Tweet.tsx
+++ components/Tweet/Tweet.tsx
@@ -3,18 +3,48 @@ import React from 'react'
 import tweets from '../../assets/data/tweets';
 import { styles } from './styles';
 import { TweetProps } from './types';
+import { Entypo, EvilIcons } from '@expo/vector-icons';
+
+
+type IconButtonProps = {
+    icon: React.ComponentProps<typeof EvilIcons>['name'];
+    text?: string | number;
+}
+const IconButton = ({ icon, text }: IconButtonProps) => {
+
+    return (
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <EvilIcons name={icon} size={22} color="gray" />
+            <Text style={{ fontSize: 12, color: 'gray' }}>{text}</Text>
+        </View>
+    )
+}
 
 const Tweet = ({ tweet }: TweetProps) => {
 
-
-
-
     return (
         <View style={styles.container}>
-            <Image src={tweet.user.image} style={styles.userImage} />
+            <Image source={{ uri: tweet.user.image }} style={styles.userImage} />
             <View style={styles.mainContainer}>
-                <Text style={styles.name}>{tweet.user.name}</Text>
+                <View style={{ flexDirection: 'row', }}>
+                    <Text style={styles.name}>{tweet.user.name}</Text>
+                    <Text style={styles.username}>@{tweet.user.username} · 2h</Text>
+                    <Entypo name="dots-three-horizontal" size={16} color="gray" style={{
+                        marginLeft: 'auto'
+                    }}
+                    />
+                </View>
                 <Text style={styles.content}>{tweet.content}</Text>
+                {tweet.image && <Image source={{ uri: tweet.image }} style={styles.image} />}
+                <View style={styles.footer}>
+                    <IconButton icon="comment" text={tweet.numberOfComments} />
+                    <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+                    <IconButton icon="heart" text={tweet.numberOfLikes} />
+                    <IconButton icon="chart" text={tweet.impressions || 0} />
+                    <IconButton icon="share-apple" />
+
+
+                </View>
             </View>
         </View>
     )
--- components/Tweet/styles.ts
+++ components/Tweet/styles.ts
@@ -2,8 +2,6 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
     container: {
-        alignItems: 'center',
-        justifyContent: 'center',
         flexDirection: "row",
         padding: 15,
         borderBottomWidth: StyleSheet.hairlineWidth,
@@ -22,8 +20,25 @@ export const styles = StyleSheet.create({
     name: {
         fontWeight: '600',
     },
+    username: {
+        color: 'grey',
+        marginLeft: 5,
+    },
     content: {
         marginTop: 5,
         lineHeight: 20,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 16 / 9,
+        marginTop: 10,
+        borderRadius: 15,
+        marginVertical: 10,
+    },
+    footer:{
+        flexDirection: 'row',
+        marginVertical: 5,
+        justifyContent: 'space-between',
+
     }
 });
