3af49793fe69a8d3871f4cad85aa02579e1433fb
Diego Vega <dvegamed@gmail.com>
2023-06-25 02:36:27 -0400

    Add TweetScreen and dynamic links

---
+++ app/tweet/[id].tsx
@@ -0,0 +1,15 @@
+import { useSearchParams } from "expo-router";
+import { Text } from "react-native";
+import tweets from "../../assets/data/tweets";
+import { Tweet } from "../../components/Tweet";
+export default function TweetScreen() {
+    const { id } = useSearchParams();
+
+    const tweet = tweets.find((tweet) => tweet.id === id);
+
+    if (!tweet) { return <Text>Tweet not found</Text> }
+
+    return (
+        <Tweet tweet={tweet} />
+    )
+}
--- components/Tweet/Tweet.tsx
+++ components/Tweet/Tweet.tsx
@@ -1,39 +1,43 @@
-import { Image, View, Text } from 'react-native'
+import { Image, View, Text, Pressable } from 'react-native'
 import React from 'react'
 import { styles } from './styles';
 import { TweetProps } from './types';
 import { Entypo } from '@expo/vector-icons';
 import { IconButton } from './components/IconButton';
+import { Link } from 'expo-router';
 
 
 
 const Tweet = ({ tweet }: TweetProps) => {
 
     return (
-        <View style={styles.container}>
-            <Image source={{ uri: tweet.user.image }} style={styles.userImage} />
-            <View style={styles.mainContainer}>
-                <View style={{ flexDirection: 'row', }}>
-                    <Text style={styles.name}>{tweet.user.name}</Text>
-                    <Text style={styles.username}>@{tweet.user.username} · 2h</Text>
-                    <Entypo name="dots-three-horizontal" size={16} color="gray" style={{
-                        marginLeft: 'auto'
-                    }}
-                    />
-                </View>
-                <Text style={styles.content}>{tweet.content}</Text>
-                {tweet.image && <Image source={{ uri: tweet.image }} style={styles.image} />}
-                <View style={styles.footer}>
-                    <IconButton icon="comment" text={tweet.numberOfComments} />
-                    <IconButton icon="retweet" text={tweet.numberOfRetweets} />
-                    <IconButton icon="heart" text={tweet.numberOfLikes} />
-                    <IconButton icon="chart" text={tweet.impressions || 0} />
-                    <IconButton icon="share-apple" />
+        <Link href={`/tweet/${tweet.id}`} asChild>
+
+            <Pressable style={styles.container}>
+                <Image source={{ uri: tweet.user.image }} style={styles.userImage} />
+                <View style={styles.mainContainer}>
+                    <View style={{ flexDirection: 'row', }}>
+                        <Text style={styles.name}>{tweet.user.name}</Text>
+                        <Text style={styles.username}>@{tweet.user.username} · 2h</Text>
+                        <Entypo name="dots-three-horizontal" size={16} color="gray" style={{
+                            marginLeft: 'auto'
+                        }}
+                        />
+                    </View>
+                    <Text style={styles.content}>{tweet.content}</Text>
+                    {tweet.image && <Image source={{ uri: tweet.image }} style={styles.image} />}
+                    <View style={styles.footer}>
+                        <IconButton icon="comment" text={tweet.numberOfComments} />
+                        <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+                        <IconButton icon="heart" text={tweet.numberOfLikes} />
+                        <IconButton icon="chart" text={tweet.impressions || 0} />
+                        <IconButton icon="share-apple" />
 
 
+                    </View>
                 </View>
-            </View>
-        </View>
+            </Pressable>
+        </Link >
     )
 }
 
